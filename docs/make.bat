807e20f4e4a4e8f67dc88d04e0d0b7ce247c76e6
chris <chris@ac150577-6901-3246-a117-cd8547e6756f>
2009-09-09 08:14:12 +0000

    find sphinx-build in the right place on Windows too...

--- docs/make.bat
+++ docs/make.bat
@@ -2,7 +2,7 @@
 
 REM Command file for Sphinx documentation
 
-set SPHINXBUILD=sphinx-build
+set SPHINXBUILD=..\bin\sphinx-build
 set ALLSPHINXOPTS=-d _build/doctrees %SPHINXOPTS% .
 if NOT "%PAPER%" == "" (
 	set ALLSPHINXOPTS=-D latex_paper_size=%PAPER% %ALLSPHINXOPTS%
